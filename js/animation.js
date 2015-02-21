c2a636d67e9d31e30d7f4fc1ac90593b9611055a
Mikhail Cruz Andrade <jailandrade@gmail.com>
2015-02-20 21:05:31 -0600

    agregando archivo animation

---
+++ js/animation.js
@@ -0,0 +1,16 @@
+/*
+  Functions used for animation of elements in tuxideas.com
+*/
+
+
+/**
+ * Detect if an element has focus
+ * @param element
+ */
+function elementHasFocus( element ) {
+  
+}
+
+function showLabel() {
+
+}
