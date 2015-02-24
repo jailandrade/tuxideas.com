a2c91d402a740a1b65acef9ba569c2f5abd4477d
Mikhail Cruz Andrade <jailandrade@gmail.com>
2015-02-23 21:40:10 -0600

    agregando estilos

--- js/animation.js
+++ js/animation.js
@@ -1,16 +1,26 @@
-/*
-  Functions used for animation of elements in tuxideas.com
-*/
-
+/**
+ * Functions used for animation of elements in tuxideas.com
+ * @author Mikhail Cruz Andrade
+ */
 
 /**
  * Detect if an element has focus
  * @param element
+ * @returns {Boolean} elementHasFocus
  */
 function elementHasFocus( element ) {
-  
+
 }
 
+/**
+ * Show the label for the input that has focus
+ *
+ */
 function showLabel() {
+
+}
+
+
+function addClass() {
 
 }
